5a3fd91769acca1f4c6e82d3c30fc64223ac68da
Đỗ Huy Hoàng <huyhoang14901@gmail.com>
2022-01-10 15:32:38 +0700

    add all file

---
+++ app/src/main/java/com/huyhoang/tiptime/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.huyhoang.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
