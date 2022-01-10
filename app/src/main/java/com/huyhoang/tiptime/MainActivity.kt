a687a1d1f142a8de5bdc1e6819a883e41ff664ea
Đỗ Huy Hoàng <huyhoang14901@gmail.com>
2022-01-10 22:28:22 +0700

    change theme color

--- app/src/main/java/com/huyhoang/tiptime/MainActivity.kt
+++ app/src/main/java/com/huyhoang/tiptime/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.huyhoang.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.huyhoang.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,16 +21,24 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener {
             caculateTip()
         }
+
+        // Set up a key listener on the EditText field to listen for "enter" button presses
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
     }
 
     private fun caculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost == null) {
             binding.tipResult.text = ""
             return
         }
-        val tipPercentage = when(binding.tipOption.checkedRadioButtonId) {
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -42,5 +53,19 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip : Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    /**
+     * Key listener for hiding the keyboard when the "Enter" button is tapped.
+     */
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
